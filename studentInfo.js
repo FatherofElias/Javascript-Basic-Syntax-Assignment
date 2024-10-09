72b1da0c58c1aa956077053f0397dcccb45392f5
FatherofElias <supra_3_8@hotmail.com>
2024-10-09 09:33:33 -0500

    assignment1

---
+++ studentInfo.js
@@ -0,0 +1,11 @@
+// Declare variables to store student information
+let studentName = "John Doe"; // John Doe is the assigned value
+let studentAge = 16;// 16 is the value assigned
+let studentGrade = "10th Grade"; // '10th grade' is the value assigned
+let studentAttendanceStatus = true; // true means present, false means absent and true is the assigned value
+
+// Output the student information
+console.log("Name: " + studentName);
+console.log("Age: " + studentAge);
+console.log("Grade: " + studentGrade);
+console.log("Attendance Status: " + (studentAttendanceStatus ? "Present" : "Absent"));
