fac784e40a5a7e56e4c3e9ff0a11e9564c5968bc
FatherofElias <supra_3_8@hotmail.com>
2024-10-09 09:39:43 -0500

    update

---
+++ basicCalculator.js
@@ -0,0 +1,10 @@
+// Declare variables to store two numeric values
+let num1 = 10; // assigned value
+let num2 = 5; // assigned value
+
+
+// Perform arithmetic operations and display the results
+console.log("Sum:", num1 + num2);
+console.log("Difference:", num1 - num2);
+console.log("Product:", num1 * num2);
+console.log("Quotient:", num1 / num2);
